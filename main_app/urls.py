a614dcc0b2cd1967e16d792e3f52fe60df90843f
KylieBeck <kylie.beck12@gmail.com>
2025-08-17 17:37:34 -0700

    completed movie model

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,8 +5,9 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('movies/', views.movie_index, name='movie-index'),
-
-    path('movies/<int:movie_id>/', views.movie_detail, name='movie-detail')
-
+    path('movies/<int:movie_id>/', views.movie_detail, name='movie-detail'),
+    path('movies/create/', views.MovieCreate.as_view(), name='movie-create'),
+    path('movies/<int:pk>/update/', views.MovieUpdate.as_view(), name='movie-update'),
+    path('movies/<int:pk>/delete/', views.MovieDelete.as_view(), name='movie-delete'),
     
 ]
