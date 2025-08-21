3501824306ad3e85161a156ea020621d9508bc9e
KylieBeck <kylie.beck12@gmail.com>
2025-08-20 17:21:24 -0700

    authorization, signin, signup

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views # Import views to connect routes to view functions
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('accounts/signup/', views.signup, name='signup'),
+
+    path('', views.Home.as_view(), name='home'),
     path('about/', views.about, name='about'),
     path('movies/', views.movie_index, name='movie-index'),
     path('movies/<int:movie_id>/', views.movie_detail, name='movie-detail'),
