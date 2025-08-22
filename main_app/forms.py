260dcd884bd585eeace4998881205cd0978a1d13
KylieBeck <kylie.beck12@gmail.com>
2025-08-22 13:56:38 -0700

    completed watchlist model and functionality

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Review
+from .models import Review, Watchlist
 
 class ReviewForm(forms.ModelForm):
     class Meta:
@@ -7,4 +7,11 @@ class ReviewForm(forms.ModelForm):
         fields = ['rating', 'comment']
         widgets = {
             'content': forms.Textarea(attrs={'rows': 4}),
+        }
+class WatchlistForm(forms.ModelForm):
+    class Meta:
+        model = Watchlist
+        fields = ['title', 'movies']  # Allow users to select movies
+        widgets = {
+            'movies': forms.CheckboxSelectMultiple(),  # Display movies as checkboxes
         }
