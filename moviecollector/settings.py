a1cb371401f9fc12ac1e533ff39c29e62b9a1762
Roger Shyaka <roshyak@gmail.com>
2025-08-25 05:20:24 -0400

    Add runtime.txt and update Heroku settings

--- moviecollector/settings.py
+++ moviecollector/settings.py
@@ -11,11 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
 import django_heroku
-
-import dj_database.url
-
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
